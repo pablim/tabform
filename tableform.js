4edbf2582fac8e78068732e43efe7e0775b50a73
Pablim <vazsk8@gmail.com>
2021-02-06 19:48:25 -0300

    Use template tag for row template; more appropriate names for element classes; refactoring

--- tableform.js
+++ tableform.js
@@ -1,4 +1,8 @@
-function tableFormAddRowTemplate(containerRef) {
+/**
+ * 
+ * @param {*} containerRef container reference
+ */
+function formTableAddRow(containerRef) {
 
 	var containers = []
 	if (containerRef?.content) {
@@ -19,7 +23,7 @@ function tableFormAddRowTemplate(containerRef) {
 				return false
 			}
 			template = document.getElementById(container.dataset.templateid)
-			if (!template){
+			if (!template) {
 				console.log('template ' + container.dataset.templateid + 'não existe')
 				return false
 			}
@@ -45,7 +49,7 @@ function tableFormAddRowTemplate(containerRef) {
 			
 			cloneRow.style.display = "table-row"; // Show row
 			cloneRow.id = container.id + "_row" + rowCount; // Id
-			cloneRow.classList.add("tbform-row-container")
+			cloneRow.classList.add("form-table-row-container")
 	
 			// Incrementa ids
 			var elementsToIncrementId = cloneRow.querySelectorAll('[id]');
@@ -79,112 +83,31 @@ function tableFormAddRowTemplate(containerRef) {
 	}
 }
 
-function deleteRow(obj) {
-	var catchRowContainer
-	var catchContainer
-
-	// loop até encontrar a row container
-	while (!obj.classList.contains("tbform-row-container")) {
-		var row = obj.parentNode;
+/**
+ * 
+ * @param {*} actionElement action element reference
+ */
+function formTableDeleteRow(actionElement) {
+	
+    // até encontrar a table
+    while (!actionElement.classList.contains("form-table")) {
+        // aproveitando para encontrar a row container
+        if (actionElement.classList.contains("form-table-row-container")) {
+            var row = actionElement
+        }
+		var actionElement = actionElement.parentNode;
 	}
+    var table = actionElement
 
 	var rowCount = table.getAttribute("data-row-count");
 	
-  	table.setAttribute("data-row-count", parseInt(rowCount) - 1);
+	table.setAttribute("data-row-count", parseInt(rowCount) - 1);
 
-  	var rowIndex = obj.rowIndex;
+	var rowIndex = row.rowIndex;
 	table.deleteRow(rowIndex);
 	
 	// Armazena cada número de linha localmente.
-  	localStorage.setItem(table.id + 'RowCount', parseInt(rowCount)-1);
-
-	// Começa da linha deletada
-	for (i=rowIndex; i<table.rows.length; i++) {
-
-		// Linhas depois da excluída
-		var rowCountElement = table.rows[i].querySelector('[data-row-number]');
-		if (rowCountElement != null) {
-			if (typeof rowCountElement.value === "undefined") {
-				rowCountElement.innerHTML = parseInt(rowCountElement.innerHTML) - 1;
-			} else {
-				rowCountElement.value = parseInt(rowCountElement.value) - 1;
-			}
-		}
-
-	}
-}
-
-
-
-function addRow(table) {
-
-	var table = document.getElementById(table); // find table to append to
-
-	var row = table.rows[1]; // row model
-	var rowCount = table.getAttribute("data-row-count");
-
-  	var cloneRow = row.cloneNode(true); // copy children too
-
-  	// Incrementa elementos de contagem.
-	elementCount = cloneRow.querySelectorAll('[data-row-number]');
-	for(var i=0; i<elementCount.length; i++) {
-		if (elementCount[i] != null) {
-			if (typeof elementCount[i].value === "undefined") {
-				elementCount[i].innerHTML = parseInt(rowCount);
-			}else {
-	      		elementCount[i].value = parseInt(rowCount);
-	      	}
-		}
-	}
-  	table.setAttribute("data-row-count", parseInt(rowCount) + 1);
-  	
-  	cloneRow.style.display = "table-row"; // Show row
-  	cloneRow.id = table + "row" + rowCount; // Id
-
-  	// Incrementa ids
-  	var elementsToIncrementId = cloneRow.querySelectorAll('[id]');
-  	for (var i=0; i<elementsToIncrementId.length; i++) {
-  		elementsToIncrementId[i].id = elementsToIncrementId[i].id + rowCount;
-    }
-  	
-  	// Incrementa names
-  	var elementsToIncrementName = cloneRow.querySelectorAll('[name]');
-  	for (var i=0; i<elementsToIncrementName.length; i++) {
-  		var name = elementsToIncrementName[i].name;
-  		var char = name.indexOf("#");
-  		elementsToIncrementName[i].name = name.replace("#", rowCount);
-    }
-  	
-  	var elementsToEnabled = cloneRow.querySelectorAll('[disabled]');
-  	for (var i=0; i<elementsToEnabled.length; i++) {
-  		if (elementsToEnabled[i].getAttribute("data-keep-disabled") == null) {
-  			elementsToEnabled[i].disabled = false;  			
-  		}
-    }
-  	
-  	// Armazena cada número de linha localmente.
-  	localStorage.setItem(table.id + 'RowCount', rowCount);
-  	
-  	table.appendChild(cloneRow); // add new row to end of table
-}
-
-function deleteRow(table, obj) {
-
-	var table = document.getElementById(table); // find table to append to
-
-	while (obj.nodeName != "TR") {
-		obj = obj.parentNode;
-	}
-
-	var rowCount = table.getAttribute("data-row-count");
-	
-  	table.setAttribute("data-row-count", parseInt(rowCount) - 1);
-
-  	var rowIndex = obj.rowIndex;
-	table.deleteRow(rowIndex);
-	
-	// Armazena cada número de linha localmente.
-  	localStorage.setItem(table.id + 'RowCount', parseInt(rowCount)-1);
+	localStorage.setItem(table.id + 'RowCount', parseInt(rowCount)-1);
 
 	// Começa da linha deletada
 	for (i=rowIndex; i<table.rows.length; i++) {
